1fdd859eafde5d49de34ac9415abee6be089d5c5
yunjie <yunjie-li@users.noreply.github.com>
2020-09-06 11:03:08 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,5 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+git clone https://github.com/garypang13/luci-theme-edge.git package/diy/luci-theme-edge
